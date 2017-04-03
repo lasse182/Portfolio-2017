6f572c2a567ed8e12d494d6211cbdc6447ab7989
Lasse Iversen <lasse.iver@gmail.com>
2017-04-03 17:06:44 +0200

    Initial commit including react-create-app setup.

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is the repo for the frontend portfolio of Lasse Iversen
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Portfolio of Lasse Iversen
+      </div>
+    );
+  }
+}
+
+export default App;
